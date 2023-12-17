3f7c9cc069aac74520c7d51c1d3d9217491934bb
Kazuki Hosone <v.t-3904@outlook.com>
2023-12-17 22:35:24 +0900

    Windows 11 の設定を変更するバッチファイル２種を作成
    ◆ マウスポインターの設定
    ◆ Print Screen キーを押下したときに Snipping Tool を起動するかの設定

---
+++ ChangeMousePointerSettings.bat
@@ -0,0 +1,11 @@
+@echo off
+@chcp 65001 > nul
+
+rem マウスポインターの設定を変更するバッチファイル
+rem 色を黒に変更し サイズを Windows 11 設定基準の 2 に変更する
+
+rem マウスポインターの色を黒に変更
+reg add "HKEY_CURRENT_USER\Control Panel\Cursors" /ve /t "REG_SZ" /d "Windows Black" /f
+
+rem マウスポインターのサイズを変更
+reg add "HKEY_CURRENT_USER\Control Panel\Cursors" /v "CursorBaseSize" /t "REG_DWORD" /d "0x00000030" /f
---
+++ DIsableSnippingToolUsingPrintScreen.bat
@@ -0,0 +1,11 @@
+@echo off
+@chcp 65001 > nul
+
+rem PtintScreen キーを押下すると Snipping Tool が起動する設定を無効にする
+rem Windows 11 Build 22624.1546 (KB5025310) からデフォルトで ON になっている
+rem FF14 でスクリーンショットを撮るときに起動するため作成
+
+reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v "PrintScreenKeyForSnippingEnabled" /t "REG_DWORD" /d "0x00000000" /f
+
+rem ON にする場合は以下
+rem reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v "PrintScreenKeyForSnippingEnabled" /t "REG_DWORD" /d "0x00000001"
